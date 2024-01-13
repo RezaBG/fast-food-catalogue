d7d7fded1b7a012c095b2e34e60c6387d558fecd
rezaBG <rezabarzegargashti@gmail.com>
2024-01-13 15:43:04 +0100

    add header cmn - pic - basic design

---
+++ src/Header/header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="bg-size-cover bg-position-center py-5">
+        <div className="container">
+          <h1 className="text-start text-light text-appercase">
+            fast food calatogue
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
